2ac6192979eb6cea25128a1d7e87bc260c97956d
Isaias Ferreira Dias <isaiasferreiradias@gmail.com>
2021-06-19 16:13:09 -0300

    d

--- sonar.sh
+++ sonar.sh
@@ -1,6 +1,6 @@
 mvn sonar:sonar \
   -Dsonar.projectKey=enade \
-  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.host.url=http://sonarqube:9000 \
   -Dsonar.login=423e74a53bf210c27735d92fbda8596c9398b0d8
 
 
